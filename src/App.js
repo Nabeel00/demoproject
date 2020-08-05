2df34f4e80af02186aa13fb5caf6f8f722564597
Nabeel Hassan <nabeel@etechtics.com>
2020-08-05 21:53:28 +0500

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <p>You can now view demoproject in the browser.</p>
+    </div>
+  );
+}
+
+export default App;
